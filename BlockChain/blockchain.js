8ced60e6452264000aa263a7c2985f15d9b7aa18
Yuichiro Iijima <jimaichiro@gmail.com>
2019-11-12 20:02:34 +0900

    add cryptographic hash function

--- BlockChain/blockchain.js
+++ BlockChain/blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require("sha256");
+
 function BlockChain() {
   this.chain = [];
   this.pendingTransaction = [];
@@ -17,6 +19,35 @@ BlockChain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
   this.chain.push(newBlock);
 
   return newBlock;
+};
+
+BlockChain.prototype.getLastBlock = function() {
+  return this.chain[this.chain.length - 1];
+};
+
+BlockChain.prototype.createNewTransaction = function(
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient
+  };
+  this.pendingTransaction.push(newTransaction);
+  return this.getLastBlock()["index"] + 1;
+};
+
+BlockChain.prototype.hashBlock = function(
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataAsString =
+    previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+  return hash;
 };
 
 module.exports = BlockChain;
--- BlockChain/test.js
+++ BlockChain/test.js
@@ -1,6 +1,26 @@
 const BlockChain = require("./blockchain");
 const bitcoin = new BlockChain();
 
-bitcoin.createNewBlock(2389, "00IAIJIJUIGGUGUYG", "09IAIJHIFYFTUFIFDIJU");
+const previousBlockHash = "0AA0IAIJIJUIGGUGUYG";
 
-console.log(bitcoin);
+const currentBlockData = [
+  {
+    amount: 10,
+    sender: "ALICE090970FYFFYFYFIF",
+    recipient: "BOB797789790JFJFFGFJF"
+  },
+  {
+    amount: 30,
+    sender: "ALICGHIUGUGOOIGODYGDHFD",
+    recipient: "BOBTYSHGHOUHOHOHOHOHO"
+  },
+  {
+    amount: 200,
+    sender: "ALICEHJGUGUTETEEUUCVVUVUV",
+    recipient: "BOBGIUGIUGIUDRTESREAREUY"
+  }
+];
+
+const nonce = 100;
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
